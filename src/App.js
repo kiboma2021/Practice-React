4d6b1f4c7c3c2bebe09b953237612dcaabf30589
Ben Kiboma <omayioben@gmail.com>
2022-06-29 19:06:57 +0300

    Import Calculator to Main APP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 
 function App() {
-  const welcomeMsg = "Welcome to my React APP!!"
+  const welcomeMsg = "Welcome to my React APP Ben!!";
+  const hobby = "love coding";
+  const times = 50;
   return (
     <div className="App">
       <div className="Content">
         <h1>{welcomeMsg}</h1>
+        <p>I really {hobby} </p>
+        <p> I have coded using React {times} times</p>
       </div>
     </div>
   );
